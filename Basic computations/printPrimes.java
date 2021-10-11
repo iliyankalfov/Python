985292069637a0d3b94dc61b3a0ebfc3f1313307
iliyankalfov <kalfov.iliyan@yahoo.com>
2021-10-12 00:31:04 +0100

    First JAVA commit

---
+++ Basic computations/printPrimes.java
@@ -0,0 +1,17 @@
+void printPrimes(int max){
+    for (int n=2;n<max;n++){
+        boolean prime = true;
+        for (int f=2;f<=Math.sqrt(n);f++){
+            if (n%f==0)
+            {
+                prime=false;
+                break;
+            }
+            
+        }
+        if (prime)
+        {
+            System.out.println(n+" is prime");
+        }
+    }
+}
